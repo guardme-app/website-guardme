4944712e1ec6846b07778221cba9fd99f5830ef4
Luiz Fellipe Meiraitt <luiz.fellipemeiraitt@gmail.com>
2024-05-05 15:11:02 -0300

    footer #12

--- src/presentation/views/Home/components/Footer/Footer.tsx
+++ src/presentation/views/Home/components/Footer/Footer.tsx
@@ -13,40 +13,69 @@ export const Footer: React.FC = () => {
     <FooterStyles.Container>
       <Container $flexDirection="column">
         <Grid container>
-          <FooterStyles.ContainerText>
-            <span>
-              <Image
-                src={logoFooter}
-                alt="logo"
-                quality={100}
-                width={75}
-                height={107}
-              />
-              <h1>Hire the best insurance</h1>
-            </span>
-          </FooterStyles.ContainerText>
-          <FooterStyles.ContainerText>
-            <span>
-              <p>Home</p>
-              <p>Vehicles</p>
-              <p>Houses</p>
-              <p>Smartphones</p>
-            </span>
-          </FooterStyles.ContainerText>
-          <FooterStyles.ContainerText>
-            <span>
-              <h2>Promo</h2>
-              <p>10% off</p>
-            </span>
-          </FooterStyles.ContainerText>
-          <FooterStyles.ContainerText>
-            <span>
-              <h2>Suporte</h2>
-              <p>Help Center</p>
-              <p>Send us an email</p>
-              <p>Careers</p>
-            </span>
-          </FooterStyles.ContainerText>
+          <Grid
+            item
+            lg={3}
+            md={3}
+            xs={6}
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <FooterStyles.ContainerText>
+              <span>
+                <Image
+                  src={logoFooter}
+                  alt="logo"
+                  quality={100}
+                  width={75}
+                  height={107}
+                />
+                <h1>Hire the best insurance</h1>
+              </span>
+            </FooterStyles.ContainerText>
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            md={3}
+            xs={6}
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <FooterStyles.ContainerText>
+              <span>
+                <p>Home</p>
+                <p>Vehicles</p>
+                <p>Houses</p>
+                <p>Smartphones</p>
+              </span>
+            </FooterStyles.ContainerText>
+          </Grid>
+          <Grid
+            item
+            lg={3}
+            md={3}
+            xs={6}
+            flexDirection="column"
+            justifyContent="center"
+          >
+            <FooterStyles.ContainerText>
+              <span>
+                <h2>Promo</h2>
+                <p>10% off</p>
+              </span>
+            </FooterStyles.ContainerText>
+          </Grid>
+          <Grid item>
+            <FooterStyles.ContainerText>
+              <span>
+                <h2>Suporte</h2>
+                <p>Help Center</p>
+                <p>Send us an email</p>
+                <p>Careers</p>
+              </span>
+            </FooterStyles.ContainerText>
+          </Grid>
         </Grid>
       </Container>
     </FooterStyles.Container>
