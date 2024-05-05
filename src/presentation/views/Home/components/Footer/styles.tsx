3504eb49546191e5a2025b49e67c3392577eaa90
Luiz Fellipe Meiraitt <luiz.fellipemeiraitt@gmail.com>
2024-05-05 15:07:29 -0300

    develop

--- src/presentation/views/Home/components/Footer/styles.tsx
+++ src/presentation/views/Home/components/Footer/styles.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 const Container = styled.div`
   width: 100%;
-  height: 600px;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
